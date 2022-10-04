4703ec7b583c568ecf77eaef88d970344988c40d
Dmitriy <Sutyagin15086733@yandex.ru>
2022-10-04 19:32:49 +0300

    Created working code

--- Programs/Program.cs
+++ Programs/Program.cs
@@ -3,6 +3,7 @@ Console.WriteLine("How many lines do you want to enter?");
 int size = Convert.ToInt32(Console.ReadLine());
 string b = "yes";
 int count = 0;
+int k = 0;
 string?[] array = new string[size];
 for (int i = 0; i < size; i++)
 {
@@ -14,3 +15,13 @@ for (int i = 0; i < size; i++)
 }
 Console.WriteLine();
 Console.WriteLine('[' + string.Join(",", array) + ']');
+string?[] arr = new string[count];
+for (int j = 0; j < array.Length; j++)
+{
+    if (array[j]?.Length <= b.Length && k <= count)
+    {
+        arr[k] = array[j];  
+        k++;
+    }
+}
+Console.WriteLine('[' + string.Join(",", arr) + ']');
